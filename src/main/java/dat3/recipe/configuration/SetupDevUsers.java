ffad6dfdf748f3c6119adedd2f1a845fd7713f72
Viktor August Rattleff <varattleff@Viktors-MacBook-Air.local>
2024-03-06 19:48:44 +0100

    update

--- src/main/java/dat3/recipe/configuration/SetupDevUsers.java
+++ src/main/java/dat3/recipe/configuration/SetupDevUsers.java
@@ -1,9 +1,12 @@
-package dat3.security_demo.configuration;
+package dat3.recipe.configuration;
 
 import dat3.security.entity.Role;
 import dat3.security.entity.UserWithRoles;
 import dat3.security.repository.RoleRepository;
 import dat3.security.repository.UserWithRolesRepository;
+import dat3.recipe.entity.SpecialUser;
+import dat3.recipe.repository.SpecialUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,6 +40,8 @@ public class SetupDevUsers implements ApplicationRunner {
         roleRepository.save(new Role("ADMIN"));
     }
 
+    @Autowired
+    SpecialUserRepository specialUserRepository;
      /*****************************************************************************************
      IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      NEVER  COMMIT/PUSH CODE WITH DEFAULT CREDENTIALS FOR REAL
@@ -66,5 +71,9 @@ public class SetupDevUsers implements ApplicationRunner {
         userWithRolesRepository.save(user2);
         userWithRolesRepository.save(user3);
         userWithRolesRepository.save(user4);
+        SpecialUser specialUser =
+                new SpecialUser("specialUser",pwEncoder.encode(passwordUsedByAll),"s@a.dk","Anders","Hansen","Lyngby vej 23","2800","Lyngby");
+        specialUser.addRole(roleUser);
+        specialUserRepository.save(specialUser);
     }
 }
